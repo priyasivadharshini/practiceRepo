5a52d7a137f4e295bfb86b5567bda8b1c6fb5c16
priyasivadharshini <53574705+priyasivadharshini@users.noreply.github.com>
2019-08-16 19:07:54 +0530

    comitting sample java code

---
+++ MyFirstJavaClass.java
@@ -0,0 +1,26 @@
+import java.util.Locale;
+import java.util.Scanner;
+import java.time.LocalDate;
+import java.text.NumberFormat;
+public class MyFirstJavaClass{
+
+
+
+public static void main(String[]args) {
+    System.out.println("in main");
+    int a=234;
+    Locale indiaLocale = new Locale("en", "IN");
+
+    String us     = NumberFormat.getCurrencyInstance(Locale.US).format(a);
+    String india  = NumberFormat.getCurrencyInstance(indiaLocale).format(a);
+    String china  = NumberFormat.getCurrencyInstance(Locale.CHINA).format(a);
+    String france = NumberFormat.getCurrencyInstance(Locale.FRANCE).format(a);
+
+    /* Print output */
+    System.out.println("US: "     + us);
+    System.out.println("India: "  + india);
+    System.out.println("China: "  + china);
+    System.out.println("France: " + france);
+
+        }
+}
